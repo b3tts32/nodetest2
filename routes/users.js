c6752b6110f5e98c1fbf09323ea470ff3797fff2
b3tts32 <rodneybetts@gmail.com>
2015-07-08 22:13:19 -0500

    latest commit

---
+++ routes/users.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * GET userlist by username.
+ */
+router.get('/userlist/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+	var userToFind = req.params.id;
+    collection.find({username: userToFind},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;
